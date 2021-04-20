b4aad917bbcc45063372c83ea320e696bc136def
Jose <jmgayosso@mightywizards.com>
2021-04-20 12:38:46 -0500

    Fixing pay with paypal input

--- src/pages/account/account.vue
+++ src/pages/account/account.vue
@@ -39,28 +39,36 @@
             color="white"
             :rules="[rules.required]"
           )
-          q-select(
-            label="Preferred Contact Method"
-            v-model="params.preferredContactMethod"
-            outlined
-            dark
-            standout="text-accent"
-            :options="options.preferredContactMethod"
-            emit-value
-            map-options
-            color="white"
-            :rules="[rules.required]"
-          )
+          //- q-select(
+          //-   label="Preferred Contact Method"
+          //-   v-model="params.preferredContactMethod"
+          //-   outlined
+          //-   dark
+          //-   standout="text-accent"
+          //-   :options="options.preferredContactMethod"
+          //-   emit-value
+          //-   map-options
+          //-   color="white"
+          //-   :rules="[rules.required]"
+          //- )
           q-separator.full-width(dark)
           .text-weight-bold.text-white Enter your paypal link
           q-input(
-            label="Paypal email"
+            label="Paypal Link"
             v-model="params.paypalEmail"
             outlined
             dark
             standout="text-accent"
             :rules="[rules.required]"
+            prefix="https://paypal.me/"
+            hint="Press TAB to autocomplete suggested value or ESC to cancel suggestion"
           )
+              template(v-slot:append)
+                q-icon.animated-icon.cursor-pointer.linkBtn(
+                  name="open_in_new" @click="openPayPalLink"
+                  v-show="params.paypalEmail"
+                )
+                  q-tooltip Open to confirm you write your profile link correctly
           .row.bg-primary.btnSave.q-py-sm
             q-btn.full-width(
               label="Save"
@@ -120,6 +128,9 @@ export default {
   methods: {
     onSubmitForm () {
       console.log('onSubmitted', this.params)
+    },
+    openPayPalLink () {
+      window.open(`https://paypal.me/${this.params.paypalEmail}`)
     }
   }
 }
@@ -132,4 +143,7 @@ export default {
 .btnSave
   position: sticky
   bottom: 0px
+.linkBtn:hover
+  color: $accent
+
 </style>
