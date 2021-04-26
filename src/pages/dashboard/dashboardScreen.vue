5464e641f9412996d71fb5e1cbc06a9ff8e4ba59
Jose <jmgayosso@mightywizards.com>
2021-04-26 15:11:56 -0500

    Developing tab and icons with active classes

--- src/pages/dashboard/dashboardScreen.vue
+++ src/pages/dashboard/dashboardScreen.vue
@@ -1,14 +1,71 @@
 <template lang="pug">
 #container
-    .text-h2 Dashboard Screen
+    q-tab-panels.panelContainer(
+        v-model="tab"
+        animated
+        transition-prev="scale"
+        transition-next="scale"
+        class="bg-primary text-white text-center"
+    )
+        q-tab-panel(name="main")
+            .text-h6 Main
+            p Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        q-tab-panel(name="transactions")
+            .text-h6 Transactions
+            p Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        q-tab-panel(name="notifications")
+            .text-h6 Notifications
+            p Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        q-tab-panel(name="settings")
+            .text-h6 Settings
+            p Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    q-tabs.tabsContainer(
+        v-model="tab"
+        class="bg-primary text-grey-5 shadow-2"
+        align="justify"
+        indicator-color="transparent"
+        active-color="accent"
+    )
+        q-tab(name="main")
+            i.i-main(:class=" tab === 'main' ? 'tab-icon-active' : 'tab-icon'")
+        q-tab(name="transactions")
+            i.i-transactions(:class=" tab === 'transactions' ? 'tab-icon-active' : 'tab-icon'")
+        q-tab(name="notifications")
+            i.i-notification(:class=" tab === 'notifications' ? 'tab-icon-active' : 'tab-icon'")
+        q-tab(name="settings")
+            i.i-settings(:class=" tab === 'settings' ? 'tab-icon-active' : 'tab-icon'")
 </template>
 
 <script>
 export default {
-  name: 'dashboard-screens'
+  name: 'dashboard-screens',
+  data () {
+    return {
+      tab: 'main'
+    }
+  }
 }
 </script>
 
 <style lang="sass" scoped>
+#container
+    display: flex
+    flex-direction: column
+    height: 93vh
+    padding: 0px
+.panelContainer
+    flex: 1
+
+.tab-icon
+  display: inline-block
+  width: 25px
+  height: 25px
+  background: white
+
+.tab-icon-active
+  display: inline-block
+  width: 25px
+  height: 25px
+  background: $accent
 
 </style>
--- src/utils/app-icons.js
+++ src/utils/app-icons.js
@@ -5,5 +5,9 @@ export default {
   'app:find-contacts': 'img:statics/app-icons/find-contacts.png',
   'app:groups': 'img:statics/app-icons/groups.png',
   'app:login': 'img:statics/app-icons/login.png',
-  'app:my-profile': 'img:statics/app-icons/my-profile.png'
+  'app:my-profile': 'img:statics/app-icons/my-profile.png',
+  'app:notifications': 'img:statics/app-icons/notification.svg',
+  'app:transactions': 'img:statics/app-icons/transactions.svg',
+  'app:settings': 'img:statics/app-icons/settings.svg',
+  'app:offer': 'img:statics/app-icons/offer.svg'
 }
