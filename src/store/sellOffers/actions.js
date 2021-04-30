bf09c3b77f2f1fa5c57fd0b6f43dacefc16de273
Edwin Fajardo <edwin@m1ghty.io>
2021-04-30 16:07:59 -0500

    added add and del offers actions to data store

--- src/store/sellOffers/actions.js
+++ src/store/sellOffers/actions.js
@@ -13,3 +13,32 @@ export const getSellOffers = async function ({ commit }) {
     commit('general/setIsLoading', false, { root: true })
   }
 }
+
+export const addSellOffer = async function ({ commit }, params) {
+  try {
+    commit('general/setIsLoading', true, { root: true })
+    const accountName = this.getters['accounts/account']
+    const response = await this.$sellOfferApi.addSellOffer({ accountName, ...params })
+    return response
+  } catch (e) {
+    console.error('An error ocurred while trying to add sale offer', e)
+    commit('general/setErrorMsg', e.message || e, { root: true })
+    throw new Error(e)
+  } finally {
+    commit('general/setIsLoading', false, { root: true })
+  }
+}
+
+export const delSellOffer = async function ({ commit }, params) {
+  try {
+    commit('general/setIsLoading', true, { root: true })
+    const response = await this.$sellOfferApi.delSellOffer({ ...params })
+    return response
+  } catch (e) {
+    console.error('An error ocurred while trying to delete sale offer', e)
+    commit('general/setErrorMsg', e.message || e, { root: true })
+    throw new Error(e)
+  } finally {
+    commit('general/setIsLoading', false, { root: true })
+  }
+}
