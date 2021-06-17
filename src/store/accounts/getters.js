17377c11ee771e5b84bfdaa1e2a73789dba79ad6
Jose <jmgayosso@mightywizards.com>
2021-06-17 14:28:15 -0500

    Implementing pricePerSeedOnEUR

--- src/store/accounts/getters.js
+++ src/store/accounts/getters.js
@@ -16,5 +16,11 @@ export const currentFiatCurrency = ({ p2pAccount }) => {
   return p2pAccount.fiat_currency
 }
 export const pricePerSeedOnUSD = ({ currentSeedsPerUsd }) => {
-  return (1 / Number.parseFloat(currentSeedsPerUsd.replace(' SEEDS', '')).toFixed(4)).toFixed(4)
+  return Number.parseFloat(1 / Number.parseFloat(currentSeedsPerUsd.replace(' SEEDS', '')).toFixed(4)).toFixed(4)
+}
+export const pricePerSeedOnEUr = ({ currentSeedsPerUsd, fiatExchanges }) => {
+  const pricePerSeedOnUSD = Number.parseFloat(1 / Number.parseFloat(currentSeedsPerUsd.replace(' SEEDS', '')).toFixed(4)).toFixed(4)
+  const pricePerEURonUSD = fiatExchanges.rates['USD']
+  const pricePerSeedOnEUR = 1 / pricePerEURonUSD
+  return (pricePerSeedOnEUR * pricePerSeedOnUSD).toFixed(4)
 }
