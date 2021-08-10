8c062f894988883f1d7119a838f011ff629d2f89
DaniPoot <danielpoot.uc2@gmail.com>
2021-08-10 17:27:49 -0500

    Created component to display alert

---
+++ src/pages/main-tab/components/not-permissions.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+q-card.bg-red.custom-round
+  q-card-section
+    .text-body2.text-white You don't have permissions to sell
+  q-card-actions.justify-center
+    q-btn.text-white.text-sm(flat :label="$t('pages.general.close')" @click="$emit('onClose')")
+</template>
+
+<script>
+export default {
+  name: 'not-permissions'
+}
+</script>
+
+<style scope>
+.text-sm {
+  font-size: 13px;
+}
+
+.bg-red {
+  background-color: #EB5757
+}
+
+</style>
