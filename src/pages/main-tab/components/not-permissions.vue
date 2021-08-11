6d45d01140b36a0dd17012524729264a5a7811b8
DaniPoot <danielpoot.uc2@gmail.com>
2021-08-11 12:36:20 -0500

    Same styles on modal

--- src/pages/main-tab/components/not-permissions.vue
+++ src/pages/main-tab/components/not-permissions.vue
@@ -1,8 +1,16 @@
 <template lang="pug">
-q-card.q-pa-md.bg-light.custom-round
-  .row.justify-between
-    .subtitle.text-dark.q-mb-xs.text-red You don't have permissions to sell
-    green-flat-btn(flat :label="$t('pages.general.close')" @click="$emit('onClose')")
+#container
+  q-card.q-pa-md.bg-light.custom-round
+    .row.justify-between
+      .subtitle.text-dark.q-mb-xs Not allowed
+      green-flat-btn(:label="$t('pages.general.close')" v-close-popup)
+    .row.justify-center
+      .text-normal.text-dark.text-red.q-mb-md Only "resident" or "citizen" users can deposit and generate sales offers
+      q-btn.full-width(
+        label="Accept"
+        color="accent"
+        @click="$emit('onClose')"
+      ).custom-round
 </template>
 
 <script>
