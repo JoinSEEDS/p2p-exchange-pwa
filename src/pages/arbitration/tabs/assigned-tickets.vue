7039e6eb0d27b2fdad8f87a8a399e3f5a34eac07
DaniPoot <danielpoot.uc2@gmail.com>
2021-08-20 11:41:33 -0500

    Added styles, texts and sections

--- src/pages/arbitration/tabs/assigned-tickets.vue
+++ src/pages/arbitration/tabs/assigned-tickets.vue
@@ -1,40 +1,26 @@
 <template lang="pug">
 .flex.column
   q-pull-to-refresh(@refresh="refresh")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
-    ticket(:isAssignedTicket="true")
+    #tickets-empty(v-if="false")
+      skeleton-tickets
+    #no-data(v-if="false")
+      .text-h5.custom-font Any Ticket Assigned
+      .text-h4.custom-font Follow Up One
+    q-infinite-scroll.infiniteScroll
+      #containerScroll(ref="scrollTarget")
+        ticket
+        ticket
+        ticket
+        ticket
 </template>
 
 <script>
+import SkeletonTickets from '~/components/skeleton/skeletonTicket'
 import Ticket from '../components/ticket'
 
 export default {
   name: 'assigned-tickets',
-  components: { Ticket },
+  components: { Ticket, SkeletonTickets },
   methods: {
     refresh (done) {
       console.log('Refresh')
@@ -44,6 +30,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="sass">
+#containerScroll
+  overflow: auto
+  flex: 1
+  max-height: 600px
+#ticket-empty
+  flex: 1
+#no-data
+  height: 60vh !important
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+.custom-font
+  font-family: 'SF Pro Display'
 </style>
--- src/pages/arbitration/tabs/available-tickets.vue
+++ src/pages/arbitration/tabs/available-tickets.vue
@@ -1,40 +1,26 @@
 <template lang="pug">
 .flex.column
   q-pull-to-refresh(@refresh="refresh")
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
-    ticket
+    #tickets-empty(v-if="false")
+      skeleton-tickets
+    #no-data(v-if="false")
+      .text-h5.custom-font Any Tickets Available
+    q-infinite-scroll.infiniteScroll
+      #containerScroll(ref="scrollTarget")
+        ticket(:isAssignedTicket="true")
+        ticket(:isAssignedTicket="true")
+        ticket(:isAssignedTicket="true")
+        ticket(:isAssignedTicket="true")
+        ticket(:isAssignedTicket="true")
 </template>
 
 <script>
+import SkeletonTickets from '~/components/skeleton/skeletonTicket'
 import Ticket from '../components/ticket'
 
 export default {
   name: 'assigned-tickets',
-  components: { Ticket },
+  components: { Ticket, SkeletonTickets },
   methods: {
     refresh (done) {
       console.log('Refresh')
@@ -44,6 +30,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="sass">
+#containerScroll
+  overflow: auto
+  flex: 1
+  max-height: 600px
+#ticket-empty
+  flex: 1
+#no-data
+  height: 60vh !important
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+.custom-font
+  font-family: 'SF Pro Display'
 </style>
